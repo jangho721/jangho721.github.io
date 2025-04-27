92e910af038be85db84220fd2ea37f2276565389
jangho721 <93881223+jangho721@users.noreply.github.com>
2025-04-27 10:22:32 +0000

    Deploying to gh-pages from @ jangho721/jangho721.github.io@b66e959c7ae3291842c6658045ca40706efd1cd8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "Blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
